5a797fab20d680bc0574e42bdf241727534070ef
Steven Albert <steven.albert2001@gmail.com>
2023-10-22 10:01:44 +0300

    Sticky Config Options

--- android/src/main/java/com/vocsy/epub_viewer/ReaderConfig.java
+++ android/src/main/java/com/vocsy/epub_viewer/ReaderConfig.java
@@ -20,20 +20,15 @@ public class ReaderConfig {
     public ReaderConfig(Context context, String identifier, String themeColor,
                         String scrollDirection, boolean allowSharing, boolean showTts , boolean nightMode){
 
-//        config = AppUtil.getSavedConfig(context);
-//        if (config == null)
+       config = AppUtil.getSavedConfig(context);
+       if (config == null){
             config = new Config();
-        if (scrollDirection.equals("vertical")){
-            config.setAllowedDirection(Config.AllowedDirection.ONLY_VERTICAL);
-        }else if(scrollDirection.equals("horizontal")){
-            config.setAllowedDirection(Config.AllowedDirection.ONLY_HORIZONTAL);
-        }else{
-            config.setAllowedDirection(Config.AllowedDirection.VERTICAL_AND_HORIZONTAL);
+            config.setThemeColorInt(Color.parseColor(themeColor));
+            config.setNightThemeColorInt(Color.parseColor(themeColor));
+            config.setShowRemainingIndicator(true);
+            config.setShowTts(showTts);
+            config.setNightMode(nightMode);
         }
-        config.setThemeColorInt(Color.parseColor(themeColor));
-        config.setNightThemeColorInt(Color.parseColor(themeColor));
-        config.setShowRemainingIndicator(true);
-        config.setShowTts(showTts);
-        config.setNightMode(nightMode);
+
     }
 }
